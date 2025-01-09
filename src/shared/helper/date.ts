6a6f162071114604ed4d5d691e87d700612d8e8d
Nguyen-Sy <synguyentien.work@gmail.com>
2025-01-09 14:48:39 +0700

    chore: update leaderboard

--- src/shared/helper/date.ts
+++ src/shared/helper/date.ts
@@ -10,8 +10,8 @@ export const getMondayStartOfWeek = (): number => {
 export const getFriday6pmOfWeek = (): number => {
     const now = new Date();
     const day = now.getDay();
-    const diff = day <= 5 ? 5 - day : 5 - day + 7;
-    const friday = new Date(now.setDate(now.getDate() + diff));
-    friday.setHours(18, 0, 0, 0);
-    return friday.getTime();
+    const diff = day === 5 ? 0 : day + 2;
+    const lastFriday = new Date(now.setDate(now.getDate() - diff));
+    lastFriday.setHours(18, 0, 0, 0);
+    return lastFriday.getTime();
 };
