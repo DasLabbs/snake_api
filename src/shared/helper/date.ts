ce56b442fca7c30df7c6a740e33f5fb80f09439d
Nguyen-Sy <synguyentien.work@gmail.com>
2025-01-10 14:21:22 +0700

    chore: update leaderboard

--- src/shared/helper/date.ts
+++ src/shared/helper/date.ts
@@ -9,9 +9,14 @@ export const getMondayStartOfWeek = (): number => {
 
 export const getFriday6pmOfWeek = (): number => {
     const now = new Date();
-    const day = now.getDay();
-    const diff = day === 5 ? 0 : day + 2;
+    const day = now.getDay(); // Current day of the week (0 = Sunday, 6 = Saturday)
+    const hour = now.getHours(); // Current hour
+
+    // Calculate the difference to the last Friday
+    const diff = day === 5 && hour >= 18 ? 0 : day === 0 ? 2 : day + 2; // If today is Friday but before 6 PM, go back to last week
+
     const lastFriday = new Date(now.setDate(now.getDate() - diff));
+    // Set time to 6:00 PM
     lastFriday.setHours(18, 0, 0, 0);
     return lastFriday.getTime();
 };
